ebb6a265348be474f3ebbe004dcc924e0709bc63
Vance Lucas <vance@vancelucas.com>
2014-09-12 15:17:24 -0500

    Fix PostgreSQL (yay for checking data types!)

--- tests/SpotTest/Entity/Zip.php
+++ tests/SpotTest/Entity/Zip.php
@@ -18,7 +18,7 @@ class Zip extends Entity
             'id'    => ['type' => 'integer', 'primary' => true, 'autoincrement' => true],
             'code'  => ['type' => 'string', 'required' => true, 'unique' => 'code_city_state'],
             'city'  => ['type' => 'string', 'required' => true, 'unique' => 'code_city_state'],
-            'state' => ['type' => 'integer', 'required' => true, 'unique' => 'code_city_state'],
+            'state' => ['type' => 'string', 'required' => true, 'unique' => 'code_city_state'],
             'lat'   => ['type' => 'decimal', 'precision' => '10', 'scale' => 8, 'required' => true, 'index' => 'location'],
             'lng'   => ['type' => 'decimal', 'precision' => '10', 'scale' => 8,  'required' => true, 'index' => 'location'],
         ];
