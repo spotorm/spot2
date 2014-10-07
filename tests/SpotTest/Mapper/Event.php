1f03a12441769d0468c24091d455b8f4c9c40fa0
Vance Lucas <vance@vancelucas.com>
2014-10-07 09:24:47 -0500

    Fix #34 - Move Scopes to Mapper
     * This is a BREAKING change.
     * This will be released in v2.1 to minimize breakage

--- tests/SpotTest/Mapper/Event.php
+++ tests/SpotTest/Mapper/Event.php
@@ -2,9 +2,27 @@
 namespace SpotTest\Mapper;
 
 use Spot\Mapper;
+use Spot\Query;
 
 class Event extends Mapper
 {
+    /**
+     * Custom scopes applied to Spot\Query
+     *
+     * @return array
+     */
+    public function scopes()
+    {
+        return [
+            'free' => function (Query $query) {
+                return $query->where(['type' => 'free']);
+            },
+            'active' => function (Query $query) {
+                return $query->where(['status' => 1]);
+            }
+        ];
+    }
+
     /**
      * Just generate a test query so we can ensure this method is getting called
      *
