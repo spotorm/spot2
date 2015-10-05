d0353b2816491cf1ebacfb2f66ed96763899abd8
Dijky <dijky@outlook.de>
2015-10-05 02:23:04 +0200

    Add test case for explicit schema definition in the Entity. Modify SQLite test configuration to exclude this feature.

---
+++ tests/Entity/Schema/Test.php
@@ -0,0 +1,26 @@
+<?php
+namespace SpotTest\Entity\Schema;
+
+use Spot\Entity;
+use Spot\EntityInterface;
+use Spot\MapperInterface;
+use Spot\EventEmitter;
+
+/**
+ * Post
+ *
+ * @package Spot
+ */
+class Test extends Entity
+{
+    protected static $table = 'spot_test.test_schema_test';
+
+    public static function fields()
+    {
+        return [
+            'id'           => ['type' => 'integer', 'autoincrement' => true, 'primary' => true],
+            'unique'       => ['type' => 'integer', 'default' => 0, 'unique' => true],
+            'index'        => ['type' => 'integer', 'default' => 0, 'index' => true]
+        ];
+    }
+}
