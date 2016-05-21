94fd8c2ae93a62cdc36782c223dbc6ead7f54839
Vance Lucas <vance@vancelucas.com>
2016-05-20 23:38:40 -0500

    Update DBAL and PHPUnit; Query method passthrough

--- lib/Query.php
+++ lib/Query.php
@@ -799,12 +799,8 @@ class Query implements \Countable, \IteratorAggregate, \ArrayAccess, \JsonSerial
 
             // Methods on Collection
         } elseif (method_exists('\\Spot\\Entity\\Collection', $method)) {
-            if(isset($args[0])){
-            return $this->execute()->$method($args[0]);
-            } else {
-            return $this->execute()->$method();
-            }
-            
+            return call_user_func_array($method, $args);
+
             // Error
         } else {
             throw new \BadMethodCallException("Method '" . __CLASS__ . "::" . $method . "' not found");
